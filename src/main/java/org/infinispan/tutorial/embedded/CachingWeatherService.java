01900a9504fca05984cbd9dc3cd24e8134579295
Tristan Tarrant <ttarrant@redhat.com>
2015-01-29 14:12:12 +0100

    step-2/cache-put-get
    Add caching to the weather service

---
+++ src/main/java/org/infinispan/tutorial/embedded/CachingWeatherService.java
@@ -0,0 +1,23 @@
+package org.infinispan.tutorial.embedded;
+
+import org.infinispan.Cache;
+
+public abstract class CachingWeatherService implements WeatherService {
+   final private Cache<String, LocationWeather> cache;
+
+   public CachingWeatherService(Cache<String, LocationWeather> cache) {
+      this.cache = cache;
+   }
+
+   @Override
+   final public LocationWeather getWeatherForLocation(String location) {
+      LocationWeather weather = cache.get(location);
+      if (weather == null) {
+         weather = fetchWeather(location);
+         cache.put(location, weather);
+      }
+      return weather;
+   }
+
+   protected abstract LocationWeather fetchWeather(String location);
+}

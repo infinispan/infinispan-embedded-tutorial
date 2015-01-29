ebf934f0cc59f2a9a202928d397200f46154a0b4
Tristan Tarrant <ttarrant@redhat.com>
2015-01-29 16:41:48 +0100

    step-9/externalizer
    Implement a custom externalizer for our value class

--- src/main/java/org/infinispan/tutorial/embedded/LocationWeather.java
+++ src/main/java/org/infinispan/tutorial/embedded/LocationWeather.java
@@ -1,9 +1,15 @@
 package org.infinispan.tutorial.embedded;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.io.Serializable;
 
+import org.infinispan.commons.marshall.Externalizer;
+import org.infinispan.commons.marshall.SerializeWith;
 import org.infinispan.distribution.group.Grouper;
 
+@SerializeWith(LocationWeather.LWExternalizer.class)
 public class LocationWeather implements Serializable {
    final float temperature;
    final String conditions;
@@ -33,4 +39,22 @@ public class LocationWeather implements Serializable {
       }
    }
 
+   public static class LWExternalizer implements Externalizer<LocationWeather> {
+
+      @Override
+      public void writeObject(ObjectOutput output, LocationWeather object) throws IOException {
+         output.writeFloat(object.temperature);
+         output.writeUTF(object.conditions);
+         output.writeUTF(object.country);
+      }
+
+      @Override
+      public LocationWeather readObject(ObjectInput input) throws IOException, ClassNotFoundException {
+         float temperature = input.readFloat();
+         String conditions = input.readUTF();
+         String country = input.readUTF();
+         return new LocationWeather(temperature, conditions, country);
+      }
+
+   }
 }
